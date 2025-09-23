dd21c5cb8dbbc426e409e21d5a3024c2b8556977
Damian Lim <limdzy@gmail.com>
2025-09-23 17:20:57 +0800

    Update listings.json

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'dist'
-  }
+  server: { port: 3000 },
+  build: { outDir: 'dist' }
 });
